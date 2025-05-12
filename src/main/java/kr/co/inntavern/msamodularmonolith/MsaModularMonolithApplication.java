8e7125a6f0e2d1313440eb1aec7b7fba3d8f7780
co2plant <jaonz6057@wku.ac.kr>
2025-05-12 23:05:23 +0900

    Init

---
+++ src/main/java/kr/co/inntavern/msamodularmonolith/MsaModularMonolithApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.msamodularmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaModularMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaModularMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/inntavern/msamodularmonolith/MsaModularMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.msamodularmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaModularMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
